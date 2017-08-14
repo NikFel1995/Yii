eb5870971d77e25d8c4b555d81909ea66bcd4aaf
NikFel1995 <stena-55@mail.ru>
2017-08-14 21:00:14 +0500

    Run Yii Application

--- web/index.php
+++ web/index.php
@@ -1,1 +1,9 @@
-VideoSchool
+<?php
+
+    require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+    @$config=require __DIR__ . '/../config/web.php';
+
+(new yii\web\Application($config))->run();
+
+
+?>
