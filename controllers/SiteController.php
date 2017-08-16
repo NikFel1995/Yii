62fef34fa341fe414580db6dd6dc20580df47175
NikFel1995 <stena-55@mail.ru>
2017-08-16 10:46:58 +0500

    Add siteController and actionIndex

---
+++ controllers/SiteController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: NikFel
+ * Date: 16.08.2017
+ * Time: 10:43
+ */
+
+namespace app\controllers;
+
+
+use yii\web\Controller;
+
+class SiteController extends  Controller
+{
+ public  function  actionIndex()
+ {
+     return 'VideoSchool';
+ }
+}
